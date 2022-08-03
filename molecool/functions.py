a9972b545ef25850c4b241545139732b94cd3a25
Kofi Nketia Ackaah-Gyasi <ackaahgyasi09@gmail.com>
2022-08-03 00:24:08 -0500

    add open_pdb function to molecool

--- molecool/functions.py
+++ molecool/functions.py
@@ -55,3 +55,16 @@ def zen(with_attribution=True):
       quote += "\n\tTim Peters"
 
     return quote
+import numpy as np
+def open_pdb(f_loc):
+    with open(f_loc) as f:
+        data = f.readlines()
+    c = []
+    sym = []
+    for l in data:
+        if 'ATOM' in l[0:6] or 'HETATM' in l[0:6]:
+            sym.append(l[76:79].strip())
+            c2 = [float(x) for x in l[30:55].split()]
+            c.append(c2)
+    coords = np.array(c)
+    return sym, coords
